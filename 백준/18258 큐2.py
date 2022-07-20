d1a2b1050f52370a3c734015e7b0e07f2ac0373c
JIYE KIM <hyo37009@gmail.com>
2022-07-20 15:03:07 +0900

    틀렸음

--- 백준/18258 큐2.py
+++ 백준/18258 큐2.py
@@ -1,15 +1,16 @@
 '''
 18258 큐2
 map함수가 시간복잡도가 O(n)이라길래 없애도 계속 시간초과가 뜬다.
-
 '''
+import sys
 
 if __name__ == "__main__":
     elements = []
     k = int(input())
 
     for i in range(k):
-        now = list(input().split())
+        now = list(sys.stdin.readline().rstrip().split())
+        # print(now)
 
         what = now[0]
         if what == 'push':
